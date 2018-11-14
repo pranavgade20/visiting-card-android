a8590db58d9ee06c2b024038657c17ca202b8b5c
Pranav Gade <pranavgade20@gmail.com>
2018-11-14 18:19:39 +0530

    Added a feature to enable user to take a new photo for his/her visiting card

--- app/src/main/java/com/community/jboss/visitingcard/VisitingCard/VisitingCardActivity.java
+++ app/src/main/java/com/community/jboss/visitingcard/VisitingCard/VisitingCardActivity.java
@@ -1,15 +1,24 @@
 package com.community.jboss.visitingcard.VisitingCard;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.community.jboss.visitingcard.Maps.MapsActivity;
 import com.community.jboss.visitingcard.R;
@@ -18,6 +27,11 @@ import com.community.jboss.visitingcard.SettingsActivity;
 import java.util.ArrayList;
 
 public class VisitingCardActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
+    private ImageView imageView;
 
     public ArrayList<ListDetail> getListViewElements() {
         ArrayList<ListDetail> ret = new ArrayList<>();
@@ -60,7 +74,7 @@ public class VisitingCardActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                final ListView listView = (ListView) findViewById(R.id.list_view);
+                final ListView listView = findViewById(R.id.list_view);
                 ArrayList<ListDetail> list_details = getListViewElements();
                 listView.setAdapter(new ListBaseAdapter(getApplicationContext(), list_details));
 
@@ -75,6 +89,29 @@ public class VisitingCardActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new
+                        Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+
+    @Override
+    @TargetApi(23)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visiting_card);
@@ -82,6 +119,21 @@ public class VisitingCardActivity extends AppCompatActivity {
         // TODO: Add a ImageView and a number of EditText to get his/her Visiting Card details (Currently authenticated User)
 
         // TODO: Clicking the ImageView should invoke an implicit intent to take an image using camera / pick an image from the Gallery.
+        this.imageView = this.findViewById(R.id.user_image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},
+                            MY_CAMERA_PERMISSION_CODE);
+                } else {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
 
         // TODO: Replace FAB icon with a SAVE icon
         // TODO: On Click on FAB should make a network call to store the entered information in the cloud using POST method(Do this in NetworkUtils class)
@@ -101,7 +153,6 @@ public class VisitingCardActivity extends AppCompatActivity {
         });
 
         setListElements();
-
     }
 
     @Override
